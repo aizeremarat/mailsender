f6af0b2ad2de8a2f7165ecccabd8f90fadc55da5
Aizere <marataizere22@gmail.com>
2024-02-20 12:12:12 +0300

    Initial commit

--- app.js
+++ app.js
@@ -1,53 +1,46 @@
 require('dotenv').config();
 
-// Required modules
 const express = require('express');
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
+const handlebars = require('handlebars');
 const fs = require('fs');
 
-
-
-// Initialize Express app
 const app = express();
 
-// Middleware for parsing form data
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Serve static files
 app.use(express.static('public'));
 
-// GET route for serving the HTML form
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-// POST route for handling form submission
 app.post('/send-email', (req, res) => {
     const { to, subject, text } = req.body;
-
 
     if (!to || !subject || !text) {
         return res.status(400).json({ error: 'Invalid argument exception' });
     }
 
-    console.log(req.body)
     const transporter = nodemailer.createTransport({
         service: 'Gmail',
         auth: {
-            user: process.env.SMTP_EMAIL, 
-            pass: process.env.SMTP_PASSWORD 
+            user: process.env.SMTP_EMAIL,
+            pass: process.env.SMTP_PASSWORD
         }
     });
 
-      // Read the email template file
-  const emailTemplate = fs.readFileSync(__dirname + '/email-template.html', 'utf8');
+    // Read the email template file
+    const emailTemplateSource = fs.readFileSync(__dirname + '/email-template.hbs', 'utf8');
 
-  // Inject dynamic content into the template
-  const html = emailTemplate.replace(/{{subject}}/g, subject).replace(/{{text}}/g, text);
+    // Compile the template
+    const template = handlebars.compile(emailTemplateSource);
 
-    // Email message options
+    // Replace placeholders with actual data
+    const html = template({ subject, text });
+
     const mailOptions = {
         from: process.env.SMTP_EMAIL,
         to: to,
@@ -55,7 +48,6 @@ app.post('/send-email', (req, res) => {
         html: html
     };
 
-    // Send email
     transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
             console.log(error);
@@ -67,7 +59,6 @@ app.post('/send-email', (req, res) => {
     });
 });
 
-// Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
